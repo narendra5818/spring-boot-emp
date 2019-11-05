06a52a7bcbc8ac99ad6fbef440af44bdd582bb2e
narendra5818 <narendra5818@yahoo.com>
2019-11-05 23:24:33 +0530

    updated src

---
+++ src/test/java/com/ndr/employee/springbootemp/SpringBootEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndr.employee.springbootemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
