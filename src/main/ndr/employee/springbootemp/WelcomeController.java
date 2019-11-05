eb76aa20c23aa01c5e8e5909ad49cb3d6e44ba91
narendra5818 <57397952+narendra5818@users.noreply.github.com>
2019-11-05 17:56:07 +0530

    Add files via upload

---
+++ src/main/ndr/employee/springbootemp/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.ndr.employee.springbootemp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class WelcomeController {
+    
+    @RequestMapping("/hello")
+    public Object sayHello(){
+        System.out.println("Employee ------");
+        return "Customer";
+    }
+
+}
