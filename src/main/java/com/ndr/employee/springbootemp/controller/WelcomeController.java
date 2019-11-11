2d8d03c5cbdd1162e22a35cb564a9df13485430f
narendra5818 <57397952+narendra5818@users.noreply.github.com>
2019-11-11 14:46:53 +0530

    Update WelcomeController.java

--- src/main/java/com/ndr/employee/springbootemp/controller/WelcomeController.java
+++ src/main/java/com/ndr/employee/springbootemp/controller/WelcomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     
     @RequestMapping("/hello")
+    
     public Object sayHello(){
         System.out.println("Employee -----git hub-");
         return "Customer";
